212eaaefdff6f8f03f0bff3b8aef332912cc8367
Amal P Mathews <amalpmathews2003@gmail.com>
2021-06-23 19:18:35 +0530

    Create filpkart.py

---
+++ filpkart.py
@@ -0,0 +1,37 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+
+
+def get_current_price():
+	url=r"https://www.flipkart.com/safari-mosaic-check-in-luggage-30-inch/p/itm136a30fb66bba?pid=STCEWDB3WYHZHFXD&lid=LSTSTCEWDB3WYHZHFXDDJR6MH"
+	r=requests.get(url)
+	soup=BeautifulSoup(r.content,'html5lib')
+	rupee=soup.find("div",class_="_30jeq3 _16Jk6d").text
+	rupee=rupee.replace("₹","")
+	rupee=rupee.replace(",","")
+	return rupee
+
+def get_previous_prices():
+	file=open("prices.txt","r")
+	previous_price=0
+	for line in file:
+		previous_price=line
+	file.close()
+	return previous_price
+	
+def put_current_price(price):
+	file =open("prices.text","a")
+	file.write(price)
+	file.close()
+
+def main():
+	curr_price=get_price()
+	previous_price=get_previous_prices()
+	if(curr_price<previous_price):
+		print(curr_price)
+	time.sleep(5*60)
+	main()
+
+if __name__ == '__main__':
+	main()
